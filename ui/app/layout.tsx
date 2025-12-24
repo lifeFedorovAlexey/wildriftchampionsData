6280bb4b19c85d0f8c1cf36e4bfb170075e41513
alexey fedorov <lifelex2011@gmail.com>
2025-12-25 01:16:40 +0300

    fix

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -14,16 +14,28 @@ export const metadata: Metadata = {
     "Актуальная статистика Wild Rift: тир-лист чемпионов по линиям и рангам, винрейты, пики и баны. Обновляется ежедневно.",
   keywords: [
     "Wild Rift",
+    "Wild Rift статистика",
     "WR",
-    "мобильный лол",
-    "mobile legends of league",
-    "тирлист WR",
-    "wr тирлист",
-    "винрейты Wild Rift",
+    "WR статистика",
+    "WR винрейт",
+    "WR пикрейт",
+    "WR банрейт",
     "пики и баны WR",
+    "WR пики и баны",
+    "тирлист WR",
+    "WR тирлист",
+    "мета WR",
+    "топ чемпионы WR",
     "топ пики WR",
+    "винрейты Wild Rift",
+    "пикрейт Wild Rift",
+    "банрейт Wild Rift",
     "мета Wild Rift",
     "статистика Wild Rift",
+    "статистика чемпионов Wild Rift",
+    "лучшие чемпионы Wild Rift",
+    "что пикать в Wild Rift",
+    "что банить в Wild Rift",
   ],
   alternates: { canonical: "/" },
   openGraph: {
@@ -60,12 +72,38 @@ export default function RootLayout({
     <html lang="ru" suppressHydrationWarning>
       <head>
         <meta name="color-scheme" content="dark" />
+
         <link
           rel="preconnect"
           href="https://game.gtimg.cn"
           crossOrigin="anonymous"
         />
         <link rel="dns-prefetch" href="//game.gtimg.cn" />
+
+        {/* Yandex.Metrika */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106001120', 'ym');
+
+            ym(106001120, 'init', {
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+            });
+          `}
+        </Script>
+        {/* /Yandex.Metrika */}
       </head>
 
       <body>
@@ -73,9 +111,22 @@ export default function RootLayout({
           src="https://telegram.org/js/telegram-web-app.js"
           strategy="beforeInteractive"
         />
+
         <TelegramInit />
+
         {children}
+
         <SpeedInsights />
+
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/106001120"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
